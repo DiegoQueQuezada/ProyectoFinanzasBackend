3a2d2fe2d153668dbdb63e7946580f20d984c5c7
Diego Quezada <diegodavid0404@hotmail.com>
2023-05-17 22:44:59 -0500

    Creacion del proyecto

---
+++ src/main/java/com/example/proyecto_finanzas/ProyectoFinanzasApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_finanzas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinanzasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinanzasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/proyecto_finanzas/ProyectoFinanzasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_finanzas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinanzasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
